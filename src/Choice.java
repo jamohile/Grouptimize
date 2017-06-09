80bfd08a1d098bd7fc0de0c193f46208238e67b1
Jay Mohile <jamohile@gmail.com>
2017-06-09 12:38:05 -0400

    Create Choice.java
    Added constructor overload for single choices

--- src/Choice.java
+++ src/Choice.java
@@ -5,10 +5,16 @@ public class Choice {
 	public int getStrength() {
 		return strength;
 	}
-
+	//this function is used to create a Choice object based on an incoming group
 	public Choice(PersonGroup people, int strength) {
 		this.people = people;
 		this.strength = strength;
+	}
+	//this function makes calling a choice with one person more efficient
+	public Choice(Person person, int strength){
+	    PersonGroup people = new PersonGroup();
+		people.addPerson(person);
+		Choice(people, strength);
 	}
 	
 	public PersonGroup getPeople() {
